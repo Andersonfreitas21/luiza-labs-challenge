978211d574973eb881e064c68e5ea74b74210c8e
Anderson de Franklin Freitas Nogueira <afranklin@cpqd.com.b>
2021-11-09 21:40:36 -0300

    Directory structure - first commit

---
+++ src/main/java/br/com/luizalabs/schedulerequest/ScheduleRequestApplication.java
@@ -0,0 +1,13 @@
+package br.com.luizalabs.schedulerequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleRequestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luizalabs/schedulerequest/ScheduleRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luizalabs.schedulerequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
