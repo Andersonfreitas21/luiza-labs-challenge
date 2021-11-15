ddf25162971ac95d2679d253e0e994c464eb7522
Anderson de Franklin Freitas Nogueira <afranklin@cpqd.com.b>
2021-11-15 12:04:49 -0300

    update for Java 11 and tests

---
+++ src/test/java/br/com/luizalabs/schedulerequest/api/controller/v1/SchedulingRequestControllerImplTest.java
@@ -0,0 +1,259 @@
+package br.com.luizalabs.schedulerequest.api.controller.v1;
+
+import br.com.luizalabs.schedulerequest.domain.data.entity.Addressee;
+import br.com.luizalabs.schedulerequest.domain.data.entity.Scheduling;
+import br.com.luizalabs.schedulerequest.domain.data.enums.StatusOfSchedule;
+import br.com.luizalabs.schedulerequest.domain.data.enums.TypeToSend;
+import br.com.luizalabs.schedulerequest.domain.data.v1.dto.SchedulingDTO;
+import br.com.luizalabs.schedulerequest.domain.data.v1.filter.SchedulingFilter;
+import br.com.luizalabs.schedulerequest.domain.data.v1.form.AddresseeForm;
+import br.com.luizalabs.schedulerequest.domain.data.v1.form.SchedulingForm;
+import br.com.luizalabs.schedulerequest.domain.data.v1.mapper.impl.EntityMapperImpl;
+import br.com.luizalabs.schedulerequest.domain.service.v1.ScheduleRequestServiceImpl;
+import br.com.luizalabs.schedulerequest.repository.AddresseeRepository;
+import br.com.luizalabs.schedulerequest.repository.SchedulingRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.time.LocalDateTime;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.apache.catalina.connector.Response;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.DelegatingServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ContextConfiguration(classes = {SchedulingRequestControllerImpl.class})
+@ExtendWith(SpringExtension.class)
+class SchedulingRequestControllerImplTest {
+
+    @MockBean
+    private ScheduleRequestServiceImpl scheduleRequestServiceImpl;
+
+    @Autowired
+    private SchedulingRequestControllerImpl schedulingRequestControllerImpl;
+
+    @Test
+    void testFindByStatusSchedulingRequest() {
+        SchedulingRepository schedulingRepository = mock(SchedulingRepository.class);
+        when(schedulingRepository.findSchedulingByStatus((StatusOfSchedule) any(),
+                (org.springframework.data.domain.Pageable) any()))
+                .thenReturn(new PageImpl<Scheduling>(new ArrayList<Scheduling>()));
+        AddresseeRepository addresseeRepository = mock(AddresseeRepository.class);
+        ResponseEntity<List<SchedulingDTO>> actualFindByStatusSchedulingRequestResult = (new SchedulingRequestControllerImpl(
+                new ScheduleRequestServiceImpl(schedulingRepository, addresseeRepository, new EntityMapperImpl())))
+                .findByStatusSchedulingRequest(StatusOfSchedule.PENDING, null);
+        assertEquals("<200 OK OK,[],[totalElements:\"0\", Access-Control-Expose-Headers:\"totalElements\"]>",
+                actualFindByStatusSchedulingRequestResult.toString());
+        assertTrue(actualFindByStatusSchedulingRequestResult.hasBody());
+        assertEquals(HttpStatus.OK, actualFindByStatusSchedulingRequestResult.getStatusCode());
+        assertEquals(2, actualFindByStatusSchedulingRequestResult.getHeaders().size());
+        verify(schedulingRepository).findSchedulingByStatus((StatusOfSchedule) any(),
+                (org.springframework.data.domain.Pageable) any());
+    }
+
+    @Test
+    void testFindByStatusSchedulingRequest2() {
+        Addressee addressee = new Addressee();
+        addressee.setEmail("jane.doe@example.org");
+        addressee.setSchedules(new ArrayList<Scheduling>());
+        addressee.setContactNumber("42");
+        addressee.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        addressee.setUpdatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        addressee.setAddressee("42 Main St");
+        addressee.setUuid(UUID.randomUUID());
+
+        Scheduling scheduling = new Scheduling();
+        scheduling.setMessage("Not all who wander are lost");
+        scheduling.setType(TypeToSend.WHATSAPP);
+        scheduling.setAddressee(addressee);
+        scheduling.setStatus(StatusOfSchedule.PENDING);
+        scheduling.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling.setSendDate(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling.setUpdatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling.setUuid(UUID.randomUUID());
+
+        ArrayList<Scheduling> schedulingList = new ArrayList<Scheduling>();
+        schedulingList.add(scheduling);
+        SchedulingRepository schedulingRepository = mock(SchedulingRepository.class);
+        when(schedulingRepository.findSchedulingByStatus((StatusOfSchedule) any(),
+                (org.springframework.data.domain.Pageable) any())).thenReturn(new PageImpl<Scheduling>(schedulingList));
+        AddresseeRepository addresseeRepository = mock(AddresseeRepository.class);
+        ResponseEntity<List<SchedulingDTO>> actualFindByStatusSchedulingRequestResult = (new SchedulingRequestControllerImpl(
+                new ScheduleRequestServiceImpl(schedulingRepository, addresseeRepository, new EntityMapperImpl())))
+                .findByStatusSchedulingRequest(StatusOfSchedule.PENDING, null);
+        assertTrue(actualFindByStatusSchedulingRequestResult.hasBody());
+        assertEquals(HttpStatus.OK, actualFindByStatusSchedulingRequestResult.getStatusCode());
+        assertEquals(2, actualFindByStatusSchedulingRequestResult.getHeaders().size());
+        verify(schedulingRepository).findSchedulingByStatus((StatusOfSchedule) any(),
+                (org.springframework.data.domain.Pageable) any());
+    }
+
+    @Test
+    void testFindByStatusSchedulingRequest3() {
+        Addressee addressee = new Addressee();
+        addressee.setEmail("jane.doe@example.org");
+        addressee.setSchedules(new ArrayList<Scheduling>());
+        addressee.setContactNumber("42");
+        addressee.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        addressee.setUpdatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        addressee.setAddressee("42 Main St");
+        addressee.setUuid(UUID.randomUUID());
+
+        Scheduling scheduling = new Scheduling();
+        scheduling.setMessage("Not all who wander are lost");
+        scheduling.setType(TypeToSend.WHATSAPP);
+        scheduling.setAddressee(addressee);
+        scheduling.setStatus(StatusOfSchedule.PENDING);
+        scheduling.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling.setSendDate(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling.setUpdatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling.setUuid(UUID.randomUUID());
+
+        Addressee addressee1 = new Addressee();
+        addressee1.setEmail("jane.doe@example.org");
+        addressee1.setSchedules(new ArrayList<Scheduling>());
+        addressee1.setContactNumber("42");
+        addressee1.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        addressee1.setUpdatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        addressee1.setAddressee("42 Main St");
+        addressee1.setUuid(UUID.randomUUID());
+
+        Scheduling scheduling1 = new Scheduling();
+        scheduling1.setMessage("Not all who wander are lost");
+        scheduling1.setType(TypeToSend.WHATSAPP);
+        scheduling1.setAddressee(addressee1);
+        scheduling1.setStatus(StatusOfSchedule.PENDING);
+        scheduling1.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling1.setSendDate(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling1.setUpdatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling1.setUuid(UUID.randomUUID());
+
+        ArrayList<Scheduling> schedulingList = new ArrayList<Scheduling>();
+        schedulingList.add(scheduling1);
+        schedulingList.add(scheduling);
+        SchedulingRepository schedulingRepository = mock(SchedulingRepository.class);
+        when(schedulingRepository.findSchedulingByStatus((StatusOfSchedule) any(),
+                (org.springframework.data.domain.Pageable) any())).thenReturn(new PageImpl<Scheduling>(schedulingList));
+        AddresseeRepository addresseeRepository = mock(AddresseeRepository.class);
+        ResponseEntity<List<SchedulingDTO>> actualFindByStatusSchedulingRequestResult = (new SchedulingRequestControllerImpl(
+                new ScheduleRequestServiceImpl(schedulingRepository, addresseeRepository, new EntityMapperImpl())))
+                .findByStatusSchedulingRequest(StatusOfSchedule.PENDING, null);
+        assertTrue(actualFindByStatusSchedulingRequestResult.hasBody());
+        assertEquals(HttpStatus.OK, actualFindByStatusSchedulingRequestResult.getStatusCode());
+        assertEquals(2, actualFindByStatusSchedulingRequestResult.getHeaders().size());
+        verify(schedulingRepository).findSchedulingByStatus((StatusOfSchedule) any(),
+                (org.springframework.data.domain.Pageable) any());
+    }
+
+    @Test
+    void testFindByStatusSchedulingRequest4() {
+        ScheduleRequestServiceImpl scheduleRequestServiceImpl = mock(ScheduleRequestServiceImpl.class);
+        when(scheduleRequestServiceImpl.find((StatusOfSchedule) any(), (org.springframework.data.domain.Pageable) any()))
+                .thenReturn(new PageImpl<SchedulingDTO>(new ArrayList<SchedulingDTO>()));
+        ResponseEntity<List<SchedulingDTO>> actualFindByStatusSchedulingRequestResult = (new SchedulingRequestControllerImpl(
+                scheduleRequestServiceImpl)).findByStatusSchedulingRequest(StatusOfSchedule.PENDING, null);
+        assertEquals("<200 OK OK,[],[totalElements:\"0\", Access-Control-Expose-Headers:\"totalElements\"]>",
+                actualFindByStatusSchedulingRequestResult.toString());
+        assertTrue(actualFindByStatusSchedulingRequestResult.hasBody());
+        assertEquals(HttpStatus.OK, actualFindByStatusSchedulingRequestResult.getStatusCode());
+        assertEquals(2, actualFindByStatusSchedulingRequestResult.getHeaders().size());
+        verify(scheduleRequestServiceImpl).find((StatusOfSchedule) any(), (org.springframework.data.domain.Pageable) any());
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testDelete() throws Exception {
+        doNothing().when(this.scheduleRequestServiceImpl).delete((String) any());
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/api/v1/schedule/{uuid}",
+                "01234567-89AB-CDEF-FEDC-BA9876543210");
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(this.schedulingRequestControllerImpl)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    void testDelete2() throws Exception {
+        doNothing().when(this.scheduleRequestServiceImpl).delete((String) any());
+        MockHttpServletRequestBuilder deleteResult = MockMvcRequestBuilders.delete("/api/v1/schedule/{uuid}",
+                "01234567-89AB-CDEF-FEDC-BA9876543210");
+        deleteResult.contentType("Not all who wander are lost");
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(this.schedulingRequestControllerImpl)
+                .build()
+                .perform(deleteResult);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    void testFilter() {
+        SchedulingFilter schedulingFilter = new SchedulingFilter();
+        assertNull(this.schedulingRequestControllerImpl.filter(schedulingFilter, true, 1, 3, new String[]{"Properties"},
+                Sort.Direction.ASC,
+                new DelegatingServerHttpResponse(new DelegatingServerHttpResponse(new DelegatingServerHttpResponse(
+                        new DelegatingServerHttpResponse(new ServletServerHttpResponse(new Response())))))));
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void findByStatusSchedulingRequest() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void testCreate() throws Exception {
+        SchedulingForm schedulingForm = new SchedulingForm();
+        schedulingForm.setAddressee(new AddresseeForm("42 Main St", "jane.doe@example.org", "42"));
+        schedulingForm.setMessage("Not all who wander are lost");
+        schedulingForm.setSendDate(null);
+        String content = (new ObjectMapper()).writeValueAsString(schedulingForm);
+        MockHttpServletRequestBuilder postResult = MockMvcRequestBuilders.post("/api/v1/schedule");
+        MockHttpServletRequestBuilder requestBuilder = postResult.param("toSend", String.valueOf(TypeToSend.WHATSAPP))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(this.schedulingRequestControllerImpl)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().is(400));
+    }
+
+
+}
---
+++ src/test/java/br/com/luizalabs/schedulerequest/domain/service/v1/ScheduleRequestServiceImplTest.java
@@ -0,0 +1,176 @@
+package br.com.luizalabs.schedulerequest.domain.service.v1;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import br.com.luizalabs.schedulerequest.domain.data.entity.Addressee;
+import br.com.luizalabs.schedulerequest.domain.data.entity.Scheduling;
+import br.com.luizalabs.schedulerequest.domain.data.enums.StatusOfSchedule;
+import br.com.luizalabs.schedulerequest.domain.data.enums.TypeToSend;
+import br.com.luizalabs.schedulerequest.domain.data.v1.dto.SchedulingDTO;
+import br.com.luizalabs.schedulerequest.domain.data.v1.form.AddresseeForm;
+import br.com.luizalabs.schedulerequest.domain.data.v1.form.SchedulingForm;
+import br.com.luizalabs.schedulerequest.domain.data.v1.mapper.EntityMapper;
+import br.com.luizalabs.schedulerequest.exception.NotFoundException;
+import br.com.luizalabs.schedulerequest.repository.AddresseeRepository;
+import br.com.luizalabs.schedulerequest.repository.SchedulingRepository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ContextConfiguration(classes = {ScheduleRequestServiceImpl.class})
+@ExtendWith(SpringExtension.class)
+class ScheduleRequestServiceImplTest {
+    @MockBean
+    private AddresseeRepository addresseeRepository;
+
+    @MockBean
+    private EntityMapper entityMapper;
+
+    @Autowired
+    private ScheduleRequestServiceImpl scheduleRequestServiceImpl;
+
+    @MockBean
+    private SchedulingRepository schedulingRepository;
+
+    @Test
+    void testCreate() {
+        Addressee addressee = new Addressee();
+        addressee.setEmail("jane.doe@example.org");
+        addressee.setSchedules(new ArrayList<Scheduling>());
+        addressee.setContactNumber("42");
+        addressee.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        addressee.setUpdatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        addressee.setAddressee("42 Main St");
+        addressee.setUuid(UUID.randomUUID());
+
+        Scheduling scheduling = new Scheduling();
+        scheduling.setMessage("Not all who wander are lost");
+        scheduling.setType(TypeToSend.WHATSAPP);
+        scheduling.setAddressee(addressee);
+        scheduling.setStatus(StatusOfSchedule.PENDING);
+        scheduling.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling.setSendDate(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling.setUpdatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling.setUuid(UUID.randomUUID());
+        when(this.schedulingRepository.save((Scheduling) any())).thenReturn(scheduling);
+
+        Addressee addressee1 = new Addressee();
+        addressee1.setEmail("jane.doe@example.org");
+        addressee1.setSchedules(new ArrayList<Scheduling>());
+        addressee1.setContactNumber("42");
+        addressee1.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        addressee1.setUpdatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        addressee1.setAddressee("42 Main St");
+        addressee1.setUuid(UUID.randomUUID());
+
+        Scheduling scheduling1 = new Scheduling();
+        scheduling1.setMessage("Not all who wander are lost");
+        scheduling1.setType(TypeToSend.WHATSAPP);
+        scheduling1.setAddressee(addressee1);
+        scheduling1.setStatus(StatusOfSchedule.PENDING);
+        scheduling1.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling1.setSendDate(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling1.setUpdatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling1.setUuid(UUID.randomUUID());
+        SchedulingDTO schedulingDTO = new SchedulingDTO();
+        when(this.entityMapper.toDto((Scheduling) any())).thenReturn(schedulingDTO);
+        when(this.entityMapper.formToEntity((SchedulingForm) any(), (Addressee) any(), (TypeToSend) any()))
+                .thenReturn(scheduling1);
+
+        Addressee addressee2 = new Addressee();
+        addressee2.setEmail("jane.doe@example.org");
+        addressee2.setSchedules(new ArrayList<Scheduling>());
+        addressee2.setContactNumber("42");
+        addressee2.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        addressee2.setUpdatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        addressee2.setAddressee("42 Main St");
+        addressee2.setUuid(UUID.randomUUID());
+        Optional<Addressee> ofResult = Optional.<Addressee>of(addressee2);
+        when(this.addresseeRepository.findAddresseeByAddresseeAndContactNumberAndEmail((String) any(), (String) any(),
+                (String) any())).thenReturn(ofResult);
+
+        SchedulingForm schedulingForm = new SchedulingForm();
+        schedulingForm.setAddressee(new AddresseeForm("42 Main St", "jane.doe@example.org", "42"));
+        assertSame(schedulingDTO, this.scheduleRequestServiceImpl.create(schedulingForm, TypeToSend.WHATSAPP));
+        verify(this.schedulingRepository).save((Scheduling) any());
+        verify(this.entityMapper).formToEntity((SchedulingForm) any(), (Addressee) any(), (TypeToSend) any());
+        verify(this.entityMapper).toDto((Scheduling) any());
+        verify(this.addresseeRepository).findAddresseeByAddresseeAndContactNumberAndEmail((String) any(), (String) any(),
+                (String) any());
+    }
+
+    @Test
+    void testFind() {
+        when(this.schedulingRepository.findSchedulingByStatus((StatusOfSchedule) any(),
+                (org.springframework.data.domain.Pageable) any()))
+                .thenReturn(new PageImpl<Scheduling>(new ArrayList<Scheduling>()));
+        when(this.entityMapper.toDto((List<Scheduling>) any())).thenReturn(new ArrayList<SchedulingDTO>());
+        assertTrue(this.scheduleRequestServiceImpl.find(StatusOfSchedule.PENDING, null).toList().isEmpty());
+        verify(this.schedulingRepository).findSchedulingByStatus((StatusOfSchedule) any(),
+                (org.springframework.data.domain.Pageable) any());
+        verify(this.entityMapper).toDto((List<Scheduling>) any());
+    }
+
+    @Test
+    void testDelete() throws NotFoundException {
+        Addressee addressee = new Addressee();
+        addressee.setEmail("jane.doe@example.org");
+        addressee.setSchedules(new ArrayList<Scheduling>());
+        addressee.setContactNumber("42");
+        addressee.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        addressee.setUpdatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        addressee.setAddressee("42 Main St");
+        addressee.setUuid(UUID.randomUUID());
+
+        Scheduling scheduling = new Scheduling();
+        scheduling.setMessage("Not all who wander are lost");
+        scheduling.setType(TypeToSend.WHATSAPP);
+        scheduling.setAddressee(addressee);
+        scheduling.setStatus(StatusOfSchedule.PENDING);
+        scheduling.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling.setSendDate(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling.setUpdatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling.setUuid(UUID.randomUUID());
+        Optional<Scheduling> ofResult = Optional.<Scheduling>of(scheduling);
+
+        Addressee addressee1 = new Addressee();
+        addressee1.setEmail("jane.doe@example.org");
+        addressee1.setSchedules(new ArrayList<Scheduling>());
+        addressee1.setContactNumber("42");
+        addressee1.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        addressee1.setUpdatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        addressee1.setAddressee("42 Main St");
+        addressee1.setUuid(UUID.randomUUID());
+
+        Scheduling scheduling1 = new Scheduling();
+        scheduling1.setMessage("Not all who wander are lost");
+        scheduling1.setType(TypeToSend.WHATSAPP);
+        scheduling1.setAddressee(addressee1);
+        scheduling1.setStatus(StatusOfSchedule.PENDING);
+        scheduling1.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling1.setSendDate(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling1.setUpdatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        scheduling1.setUuid(UUID.randomUUID());
+        when(this.schedulingRepository.saveAndFlush((Scheduling) any())).thenReturn(scheduling1);
+        when(this.schedulingRepository.findById((UUID) any())).thenReturn(ofResult);
+        this.scheduleRequestServiceImpl.delete("01234567-89AB-CDEF-FEDC-BA9876543210");
+        verify(this.schedulingRepository).findById((UUID) any());
+        verify(this.schedulingRepository).saveAndFlush((Scheduling) any());
+    }
+}
+
